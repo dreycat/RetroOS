21a2ffdec7d0c945cc54e3e5333ef95cb282e87d
dreycat <dreycat@yandex.ru>
2020-01-27 19:17:28 +0500

    add FullScreen component

---
+++ src/components/FullScreen/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from './FullScreen.module.css';
+
+import { ReactComponent as SquareIcon } from './img/square.svg';
+
+const toggleFullscreen = () => {
+  try {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const FullScreen = () => (
+  <div className={styles.main} onClick={toggleFullscreen}>
+    <SquareIcon width={16} height={16} />
+  </div>
+);
+
+export default FullScreen;
--- src/components/Taskbar/index.tsx
+++ src/components/Taskbar/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import Clock from '../Clock';
+import FullScreen from '../FullScreen';
 import styles from './Taskbar.module.css';
 
 const Taskbar = () => (
   <div className={styles.main}>
+    <div className={styles.expander} />
     <Clock />
+    <FullScreen />
   </div>
 );
 
