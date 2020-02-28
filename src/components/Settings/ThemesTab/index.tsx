8ecfedf2ddf6e95efb12e253b832ae5a275dbd06
dreycat <dreycat@yandex.ru>
2020-02-28 13:36:10 +0500

    refactor: move preload images

--- src/components/Settings/ThemesTab/index.tsx
+++ src/components/Settings/ThemesTab/index.tsx
@@ -3,9 +3,19 @@ import React from 'react';
 import { getCurrentTheme, setTheme, themes } from '../../../utils/themes';
 import capitalize from '../../../utils/capitalize';
 import { Themes } from '../../../types';
+import preloadImages from '../../../utils/preloadImages';
 import styles from './ThemesTab.module.css';
 
+const images = [
+  '/images/wallpapers/wp0.jpg',
+  '/images/wallpapers/wp1.jpg',
+  '/images/wallpapers/wp2.jpg',
+  '/images/wallpapers/wp3.jpg'
+];
+
 const ThemesTab = () => {
+  preloadImages(images);
+
   return (
     <fieldset className={styles.fieldset}>
       <legend className={styles.legend}>Themes:</legend>
--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
 
 import ThemesTab from './ThemesTab';
-import preloadImages from '../../utils/preloadImages';
 
-const images = [
-  '/images/wallpapers/wp0.jpg',
-  '/images/wallpapers/wp1.jpg',
-  '/images/wallpapers/wp2.jpg',
-  '/images/wallpapers/wp3.jpg'
-];
-
-const Settings = () => {
-  preloadImages(images);
-
-  return <ThemesTab />;
-};
+const Settings = () => <ThemesTab />;
 
 export default Settings;
