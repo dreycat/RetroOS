28d36140c7c10159585e67208b97f422f90a2492
dreycat <dreycat@yandex.ru>
2020-07-14 19:43:12 +0500

    fix: playlist

--- src/components/AudioPlayer/playlist.ts
+++ src/components/AudioPlayer/playlist.ts
@@ -4,59 +4,59 @@ const playlist: ITrack[] = [
   {
     id: 0,
     name: 'SomaFM: Lush',
-    url: 'http://ice2.somafm.com/lush-128-mp3',
+    url: 'https://ice2.somafm.com/lush-128-mp3',
     title: 'Sensuous and mellow vocals, mostly female, with an electronic influence',
-    isRadio: true
+    isRadio: true,
   },
   {
     id: 1,
     name: 'SomaFM: Groove Salad',
-    url: 'http://ice6.somafm.com/groovesalad-256-mp3',
+    url: 'https://ice6.somafm.com/groovesalad-256-mp3',
     title: 'A nicely chilled plate of ambient/downtempo beats and grooves',
-    isRadio: true
+    isRadio: true,
   },
   {
     id: 2,
     name: 'SomaFM: The Trip',
-    url: 'http://ice2.somafm.com/thetrip-128-mp3',
+    url: 'https://ice2.somafm.com/thetrip-128-mp3',
     title: 'Progressive house / trance. Tip top tunes',
-    isRadio: true
+    isRadio: true,
   },
   {
     id: 3,
     name: 'SomaFM: Suburbs of Goa',
-    url: 'http://ice4.somafm.com/suburbsofgoa-128-mp3',
+    url: 'https://ice4.somafm.com/suburbsofgoa-128-mp3',
     title: 'Desi-influenced Asian world beats and beyond',
-    isRadio: true
+    isRadio: true,
   },
   {
     id: 4,
     name: 'SomaFM: Mission Control',
-    url: 'http://ice2.somafm.com/missioncontrol-128-mp3',
+    url: 'https://ice2.somafm.com/missioncontrol-128-mp3',
     title: 'Celebrating NASA and Space Explorers everywhere',
-    isRadio: true
+    isRadio: true,
   },
   {
     id: 5,
     name: 'SomaFM: Drone Zone',
-    url: 'http://ice2.somafm.com/dronezone-128-mp3',
+    url: 'https://ice2.somafm.com/dronezone-128-mp3',
     title: 'Served best chilled, safe with most medications. Atmospheric textures with minimal beats',
-    isRadio: true
+    isRadio: true,
   },
   {
     id: 6,
     name: 'SomaFM: Space Station Soma',
-    url: 'http://ice4.somafm.com/spacestation-128-mp3',
+    url: 'https://ice4.somafm.com/spacestation-128-mp3',
     title: 'Tune in, turn on, space out. Spaced-out ambient and mid-tempo electronica',
-    isRadio: true
+    isRadio: true,
   },
   {
     id: 7,
     name: 'SoundHelix: Song 7',
     url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3',
     title: 'test song',
-    isRadio: false
-  }
+    isRadio: false,
+  },
 ];
 
 export default playlist;
