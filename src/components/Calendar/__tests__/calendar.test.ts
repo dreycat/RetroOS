e4d89c3c39d2596f98ec3e8f5f7262ba2372df20
dreycat <dreycat@yandex.ru>
2020-09-28 16:38:51 +0500

    changed the calendar test

--- src/components/Calendar/__tests__/calendar.test.ts
+++ src/components/Calendar/__tests__/calendar.test.ts
@@ -1,17 +1,26 @@
+import sinon from 'sinon';
 import Calendar from '../Calendar';
 import presenter from '../presenter';
 
+let clock: any;
+
+beforeEach(() => {
+  clock = sinon.useFakeTimers(new Date('2020-12-12').getTime());
+});
+
+afterEach(() => {
+  clock.restore();
+});
+
 describe('calendar:', () => {
-  it('should return current page', () => {
+  it('should return correct format', () => {
     const calendar = new Calendar(presenter);
     const page = calendar.currentPage();
 
-    expect(page).toEqual(
-      expect.objectContaining({
-        currentMonth: expect.any(Array),
-        prevMonth: expect.any(Array),
-        nextMonth: expect.any(Array),
-      })
-    );
+    expect(page.date.getFullYear()).toBe(2020);
+    expect(page.date.getMonth()).toBe(11);
+    expect(page.prevMonth).toHaveLength(2);
+    expect(page.currentMonth).toHaveLength(31);
+    expect(page.nextMonth).toHaveLength(9);
   });
 });
